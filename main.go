8b1a3b9dd7ae5cc6c973ef296e27100d03311f88
Argentusz <A4rgentum@yandex.ru>
2022-05-13 19:27:08 +0300

    Fifth experiment

--- main.go
+++ main.go
@@ -43,6 +43,13 @@ func PickingABalls(A, B *Player) {
 	B.ChoicesLog = append(B.ChoicesLog, B.Current)
 }
 
+func PickingABBalls(A, B *Player) {
+	A.Current = RandomZeroOne(float32(A.Balls[0]) / float32(A.Balls[0]+A.Balls[1]))
+	B.Current = RandomZeroOne(float32(B.Balls[0]) / float32(B.Balls[0]+B.Balls[1]))
+	A.ChoicesLog = append(A.ChoicesLog, A.Current)
+	B.ChoicesLog = append(B.ChoicesLog, B.Current)
+}
+
 func Judge(A, B *Player) bool {
 	if A.Current == B.Current {
 		A.Score += 2
@@ -90,6 +97,25 @@ func JudgeWithABallsPunished(A, B *Player) bool {
 		A.Score -= 3
 		B.Score += 3
 		A.Balls[1] -= 3
+	}
+	return false
+}
+
+func JudgeWithABBalls(A, B *Player) bool {
+	if A.Current == B.Current {
+		A.Score += 2
+		B.Score -= 2
+		A.Balls[A.Current] += 2
+		return true
+	}
+	if A.Current == 0 {
+		A.Score -= 1
+		B.Score += 1
+		B.Balls[1] += 1
+	} else {
+		A.Score -= 3
+		B.Score += 3
+		B.Balls[0] += 3
 	}
 	return false
 }
@@ -151,13 +177,36 @@ func fourth() {
 	fmt.Println("Математическое Ожидание игрока A: -0.25")
 }
 
+func fifth() {
+	var A, B Player
+	A.Balls = append(A.Balls, 10, 10)
+	B.Balls = append(B.Balls, 10, 10)
+	for i := 0; i < 100; i++ {
+		PickingABBalls(&A, &B)
+		JudgeWithABBalls(&A, &B)
+	}
+	fmt.Println("Итог:")
+	fmt.Println("Игрок А: ", A.Score, "Среднее значение: ", float64(A.Score)/100)
+	fmt.Println("Игрок B: ", B.Score, "Среднее значение: ", float64(B.Score)/100)
+	fmt.Println("Выборы игрока А: ", A.ChoicesLog)
+	fmt.Println("Выборы игрока B: ", B.ChoicesLog)
+	fmt.Println("Математическое Ожидание игрока A: -0.25")
+}
+
 func main() {
 	fmt.Println("Первый эксперимент:")
 	first()
+	fmt.Println()
 	fmt.Println("Второй эксперимент:")
 	second()
+	fmt.Println()
 	fmt.Println("Третий эксперимент:")
 	third()
+	fmt.Println()
 	fmt.Println("Четвёртый эксперимент:")
 	fourth()
+	fmt.Println()
+	fmt.Println("Пятый эксперимент:")
+	fifth()
+	fmt.Println()
 }
